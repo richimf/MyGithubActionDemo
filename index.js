841dd40155a40e70b92de08427f94fbed916a35f
Ricardo Montesinos <ricardo.montesinos@payclip.com>
2021-02-02 14:28:19 -0600

    update code

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const github = require('@actions/github')
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    //const nameToGreet = core.getInput('who-to-greet');
+    const titleRegex = core.getInput('title-regex');
+    //console.log(`Hello ${nameToGreet}!`);
     // const time = (new Date()).toTimeString();
     // core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
@@ -13,7 +14,7 @@ try {
     //console.log(`The event payload: ${payload}`);
     title = github.context!.payload!.pull_request!.title;
 
-    if (!title.match(new RegExp(titleRegex, titleRegexFlags))) {
+    if (!title.match(new RegExp(titleRegex, ''))) {
       core.setFailed(errorMessage);
     } else {
       core.setOutput("time", "All good :P");
