b9bcfb14f26966ef7481ee89f7fb4c6eb1d6ca94
Ricardo Montesinos <6757869+richimf@users.noreply.github.com>
2021-02-02 15:41:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const github = require('@actions/github')
 try {
     // const regex = core.getInput('title-regex');
     const regex = core.getInput('title-regex', {required: true});  
-    const regexFlags = core.getInput('title-regex-flags') || 'g';
-    // const title = github.context.eventName;
+    const errorMessage = core.getInput('error-message', {required: true}) || 'Error';  
+    //const regexFlags = core.getInput('title-regex-flags') || 'g';
+    // const title = github.context.eventName; 
     const title = github.context.payload.pull_request.title
     console.log(`title is ${title}`);
+    console.log(`errorMessage is ${errorMessage}`);
 
-
-    if (!title.match(new RegExp(regex, regexFlags))) {
-      core.setFailed("No funciona");
+    if (!title.match(new RegExp(regex, ''))) {
+      core.setFailed(errorMessage);
     } else {
       core.setOutput("message", "All good :P");
     }
