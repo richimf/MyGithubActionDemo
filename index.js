61d6afa83da950c662f7f5d83eb3ce75995dfc6a
Ricardo Montesinos <ricardo.montesinos@payclip.com>
2021-02-02 14:44:13 -0600

    update code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     //console.log(`The event payload: ${payload}`);
-    const title = github.context!.payload!.pull_request!.title;
+    const title = github.context.eventName;
 
     if (!title.match(new RegExp(titleRegex, titleRegexFlags))) {
       core.setFailed(errorMessage);
