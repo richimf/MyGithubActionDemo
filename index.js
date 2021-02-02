63219ba824d3b48dcc4777148e0a2f7c08ef112c
Ricardo Montesinos <ricardo.montesinos@payclip.com>
2021-02-02 14:52:50 -0600

    update actions

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    //const nameToGreet = core.getInput('who-to-greet');
-    const titleRegex = core.getInput('title-regex');
-    const titleRegexFlags = core.getInput('title-regex-flags') || 'g';
-    //console.log(`Hello ${nameToGreet}!`);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
+    const regex = core.getInput('title-regex');
+    const regexFlags = core.getInput('title-regex-flags') || 'g';
     const title = github.context.eventName;
 
-    if (!title.match(new RegExp(titleRegex, titleRegexFlags))) {
-      core.setFailed(errorMessage);
+    if (!title.match(new RegExp(regex, regexFlags))) {
+      core.setFailed("No funciona");
     } else {
       core.setOutput("message", "All good :P");
     }
